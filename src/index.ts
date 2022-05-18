f891e4dba52fbed8131ebd5de1861c62c7445668
hanjy-slowturtle <h920915@gmail.com>
2022-05-18 23:15:40 +0900

    ~ #5.8 Conclusions

--- src/index.ts
+++ src/index.ts
@@ -24,5 +24,48 @@ class Block implements BlockShape {
   }
 }
 
+class Blockchain {
+  private blocks: Block[];
+
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) {
+      return "";
+    }
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    const copyOfBlocks: Block[] = [];
+    this.blocks.forEach((block, _) => {
+      copyOfBlocks.push({ ...block });
+    });
+    return copyOfBlocks;
+  }
+}
+
 const b = new Block("prevHash", 1, "data");
 console.log(b);
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("1");
+blockchain.addBlock("2");
+blockchain.addBlock("3");
+
+blockchain.getBlocks().push(new Block("x", 0, "x"));
+blockchain.getBlocks()[0].data = "x";
+
+console.log(blockchain.getBlocks());
